4ec32a4ff060a3f3bdfb8a54b32d4987b2d05af2
Stanislav Cherkasov <Stanislav_Cherkasov@epam.com>
2019-05-04 13:49:56 +0300

    fix bash script

--- files/ebs-nvme-mapping.sh
+++ files/ebs-nvme-mapping.sh
@@ -4,10 +4,8 @@ PATH="${PATH}:/usr/sbin"
 
 for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' )
 do
-  mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
-  if [[ "/dev/${mapping}" == /dev/* ]]
-  then
-    ( [[ -b "${blkdev}" ]] && [[ -L "/dev/${mapping}" ]] ) || ln -s "${blkdev}" "${mapping}"
-  fi
+  # get mapping info from disk headers
+  mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g' | sed 's/\/dev\///')
+  # test if its block device and symlink does not exist. Then create symlink
+  ( [[ -b "${blkdev}" ]] && [[ ! -L "/dev/${mapping}" ]] ) && ln -s "${blkdev}" "/dev/${mapping}"
 done
-
