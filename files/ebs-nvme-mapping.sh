e3030bf2a6f8d8559e7f2e3c8aa934b7d2d7c155
Stanislav Cherkasov <Stanislav_Cherkasov@epam.com>
2019-07-18 16:19:16 +0300

    fix according to shellcheck

--- files/ebs-nvme-mapping.sh
+++ files/ebs-nvme-mapping.sh
@@ -7,5 +7,5 @@ do
   # get mapping info from disk headers
   mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g' | sed 's/\/dev\///')
   # test if its block device and symlink does not exist. Then create symlink
-  ( [[ -b "${blkdev}" ]] && [[ ! -L "/dev/${mapping}" ]] ) && ln -s "${blkdev}" "/dev/${mapping}"
+  { [[ -b "${blkdev}" ]] && [[ ! -L "/dev/${mapping}" ]]; } && ln -s "${blkdev}" "/dev/${mapping}"
 done
